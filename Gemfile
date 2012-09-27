e50e491ee975e4e1888078636d608192298413c5
Trevor Whitney <trevorjwhitney@gmail.com>
2012-09-26 23:21:18 -0600

    Added ActiveRecord

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
-source 'http://rubygems.org'
+source "http://rubygems.org"
 
 gem 'serialport'
+gem 'activerecord'
+gem 'sqlite3'
--- serial.rb
+++ serial.rb
@@ -1,4 +1,5 @@
 require 'serialport'
+require 'active_record'
 
 port_str = "/dev/ttyUSB0"
 baud = 57600
@@ -6,8 +7,28 @@ data_bits = 8
 stop_bits = 1
 parity = SerialPort::NONE
 
+ActiveRecord::Base.establish_connection({:adapter => 'sqlite3', :database => 'db.sqlite'})
+
+class Test < ActiveRecord::Base
+end
+
+puts "DB set up...connecting Serial"
+
 sp = SerialPort.new(port_str, baud, data_bits, stop_bits, parity)
 
+@value = 0
+
 while true do
-  printf "%c", sp.getc
+  char = sp.getc
+  if char == '\r' || char == '\n'
+    if @value > 0
+      puts "Value: #{value}"
+      test = Test.new
+      test.value = char.to_i
+      test.save
+      @value = 0
+    end
+  else
+    @value += char.to_i
+  end
 end
